667420e60106342333a5f288cf13d0e113c0e098
marlonsijnesael <fakiefsflip@gmail.com>
2017-11-01 21:45:12 +0100

    Update highspeedtrain.h

--- week 7/highspeedtrain.h
+++ week 7/highspeedtrain.h
@@ -5,15 +5,15 @@
 class HighSpeedTrain : vervoersmiddel
 {
 public:
-
+	
 	HighSpeedTrain(int _capacity, float _price, float _toeslag) {
 		capacity = _capacity;
 		price = _price;
 		toeslag = _toeslag;
 
-		void calculatePrice() {}
+		
 	}
-
+	void calculatePrice() {}
 	Train~();
 
 }
